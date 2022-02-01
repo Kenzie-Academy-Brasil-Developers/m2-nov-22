5f7a7d628eccf94c823a6291a275d9379129c5af
Hudson Carolino <hudson@kenzie.com.br>
2022-02-01 11:09:12 -0300

    interceptando eventos - loja kenzie

---
+++ loja-kenzie/assets/js/data_produtos.js
@@ -0,0 +1,208 @@
+const dataProdutos = [
+    {
+      "id":12,
+      "produtoNome":"MEN'S BETTER JACKET",
+      "imageUrl":"http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/mens-better-than-naked-jacket-AVMH_LC9_hero.png",
+      "preco":"200.00",
+      "precoPromocional":"190.00",
+      "oferta":true,
+      "estoque":100,
+      "categoria":"blusas"
+    },
+    {
+        "id":20,
+        "produtoNome":"Fone",
+        "imageUrl":"./assets/img/fone.jpg",
+         "preco":"150.00",
+        "precoPromocional":"150.00",
+        "oferta":false,
+        "estoque":20
+      
+     },
+     {
+        "id":200,
+         "produtoNome":"Blusa",
+         "imageUrl":"./assets/img/blusa.jpg",
+          "preco":"150.00",
+         "precoPromocional":"150.00",
+         "oferta":false,
+         "estoque":20,
+         "categoria":"blusas"
+       
+      },
+      {
+        "id":40,
+         "produtoNome":"Camiseta",
+         "imageUrl":"./assets/img/camiseta.jpg",
+          "preco":"150.00",
+         "precoPromocional":"150.00",
+         "oferta":false,
+         "estoque":20,
+         "categoria":"camisetas"
+       
+      },
+    {
+        "id":1234,
+       "produtoNome":"WOMEN'S BETTER JACKET",
+       "imageUrl":"http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/womens-better-than-naked-jacket-AVKL_NN4_hero.png",
+       "preco":"120.00",
+       "precoPromocional":"120.00",
+       "oferta":false,
+       "estoque":10,
+       "categoria":"blusas"
+    },
+    {
+        "id":423,
+       "produtoNome":"WOMEN'S SINGLE-TRACK SHOE",
+       "imageUrl":"http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/womens-single-track-shoe-ALQF_JM3_hero.png",
+       "preco":"360.00",
+       "precoPromocional":"190.00",
+       "oferta":false,
+       "estoque":20
+     },
+    {
+        "id":989,
+       "produtoNome":"Enduro Boa&reg; Hydration Pack",
+       "imageUrl":"http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/enduro-boa-hydration-pack-AJQZ_JK3_hero.png",
+       "preco":"450.00",
+       "precoPromocional":"450.00",
+       "oferta":false,
+       "estoque":150
+    },
+    {
+        "id":90,
+       "produtoNome":"MEN'S BETTER JACKET",
+       "imageUrl":"http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/mens-better-than-naked-jacket-AVMH_LC9_hero.png",
+       "preco":"380.00",
+       "precoPromocional":"380.00",
+       "oferta":false,
+       "estoque":100,
+       "categoria":"blusas"
+    },
+    {
+        "id":2019,
+       "produtoNome":"WOMEN'S BETTER JACKET",
+       "imageUrl":"http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/womens-better-than-naked-jacket-AVKL_NN4_hero.png",
+       "preco":"265.00",
+       "precoPromocional":"240.00",
+       "oferta":true,
+       "estoque":10,
+       "categoria":"blusas"
+    },
+    {
+        "id":2020,
+       "produtoNome":"WOMEN'S SINGLE-TRACK SHOE",
+       "imageUrl":"http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/womens-single-track-shoe-ALQF_JM3_hero.png",
+       "preco":"420.00",
+       "precoPromocional":"420.00",
+       "oferta":false,
+       "estoque":25
+     },
+    {
+        "id":2021,
+       "produtoNome":"Enduro Boa&reg; Hydration Pack",
+       "imageUrl":"http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/enduro-boa-hydration-pack-AJQZ_JK3_hero.png",
+       "preco":"150.00",
+       "precoPromocional":"150.00",
+       "oferta":false,
+       "estoque":20
+     
+    },
+    {
+        "id":01,
+       "produtoNome":"Camiseta",
+       "imageUrl":"./assets/img/camiseta1.jpg",
+       "preco":"265.00",
+       "precoPromocional":"240.00",
+       "oferta":true,
+       "estoque":10,
+       "categoria":"camisetas"
+    },
+    {
+        "id":02,
+       "produtoNome":"Caneca",
+       "imageUrl":"./assets/img/caneca.jpg",
+        "preco":"420.00",
+       "precoPromocional":"420.00",
+       "oferta":false,
+       "estoque":25
+     },
+    {
+        "id":03,
+       "produtoNome":"Fone",
+       "imageUrl":"./assets/img/fone.jpg",
+        "preco":"150.00",
+       "precoPromocional":"150.00",
+       "oferta":false,
+       "estoque":20
+     
+    },
+    {
+        "id":04,
+        "produtoNome":"Blusa",
+        "imageUrl":"./assets/img/blusa.jpg",
+         "preco":"150.00",
+        "precoPromocional":"150.00",
+        "oferta":false,
+        "estoque":20,
+        "categoria":"blusas"
+      
+     },
+     {
+        "id":05,
+        "produtoNome":"Camiseta",
+        "imageUrl":"./assets/img/camiseta.jpg",
+         "preco":"150.00",
+        "precoPromocional":"150.00",
+        "oferta":false,
+        "estoque":20,
+        "categoria":"camisetas"
+      
+     },
+     {
+        "id":06,
+        "produtoNome":"Blusa",
+        "imageUrl":"./assets/img/blusa.jpg",
+         "preco":"150.00",
+        "precoPromocional":"150.00",
+        "oferta":false,
+        "estoque":20,
+        "categoria":"blusas"
+      
+     },
+     {
+        "id":07,
+        "produtoNome":"Blusa",
+        "imageUrl":"./assets/img/blusa.jpg",
+         "preco":"150.00",
+        "precoPromocional":"150.00",
+        "oferta":false,
+        "estoque":0,
+        "categoria":"blusas"
+      
+     },
+     {
+      "id":07,
+      "produtoNome":"Blusa Exemplo",
+      "imageUrl":"./assets/img/blusa.jpg",
+       "preco":"150.00",
+      "precoPromocional":"150.00",
+      "oferta":false,
+      "estoque":0,
+      "categoria":"blusas"
+    
+   },
+   {
+      "id":20,
+      "produtoNome":"Fone",
+      "imageUrl":"./assets/img/fone.jpg",
+       "preco":"150.00",
+      "precoPromocional":"150.00",
+      "oferta":false,
+      "estoque":20
+    
+   },
+ ]
+
+
+ 
