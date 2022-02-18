46eaad5a7a4063ff4fc741ee5368d86cffe3a799
emanuelakenzie <manu.biq@gmail.com>
2022-02-18 14:50:31 -0300

    feat: adicionar demo hero poo

---
+++ hero-poo/assets/js/classes.js
@@ -0,0 +1,69 @@
+class Hero {
+    constructor(name, power, energy, age) {
+        this._name = name;
+        this._power = power;
+        this._energy = energy;
+        this._age = age;
+    }
+
+    get power() {
+        return this._power;
+    }
+
+    set power(value) {
+        const typesPower = ['Fire', 'Water', 'Air'];
+
+        if (typesPower.includes(value)) {
+            this._power = value;
+        }
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        if (typeof value === 'string') {
+            this._name = value;
+        }
+
+        this._name = 'Batata';
+    }
+
+    get energy() {
+        return this._energy;
+    }
+
+    set energy(value) {
+        if (typeof value === 'number') {
+            this._energy = value;
+        }
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(value) {
+        if (typeof value === 'number') {
+            this._age = value;
+        }
+    }
+
+    fight(adversery) {
+        if (this.energy > 10 && adversery.energy > 10) {
+            if (this.energy > adversery.energy) {
+                this.energy -= 5;
+                adversery.energy -= 10;
+
+                return `${this.name} ganhou a luta`;
+            }
+
+            adversery.energy -= 5;
+            this.energy -= 10;
+
+            return `${adversery.name} ganhou a luta!`;
+        }
+        return 'Vocês não estão aptos para lutar :( ';
+    }
+}
---
+++ hero-poo/assets/js/index.js
@@ -0,0 +1,70 @@
+const form = document.querySelector('form');
+
+const validateData = (data) => {
+    const dataTypes = {
+        name: 'string',
+        power: ['Fire', 'Water', 'Air'],
+        age: 'number',
+        energy: 'number',
+    };
+
+    const wrongFields = {};
+    let isValid = true;
+
+    for (const key in dataTypes) {
+        const currentType = dataTypes[key];
+        const currentValue = Number(data[key]) ? parseInt(data[key]) : data[key];
+
+        let isWrongType = false;
+
+        if (Array.isArray(currentType)) {
+            if (!currentType.includes(currentValue)) {
+                isWrongType = true;
+            }
+        } else if (typeof currentValue !== currentType) {
+            isWrongType = true;
+        }
+
+        if (isWrongType) {
+            wrongFields[key] = `The field ${key} needs to be ${currentType} type`;
+            isValid = false;
+        }
+    }
+
+    const response = {
+        validData: isValid,
+        wrongFields,
+    };
+
+    return response;
+};
+
+const createHero = (evt) => {
+    evt.preventDefault();
+
+    const inputs = evt.target;
+
+    const heroValues = {};
+
+    for (let i = 0; i < inputs.length; i++) {
+        const { name, value } = inputs[i];
+
+        if (name) {
+            heroValues[name] = value;
+        }
+    }
+
+    const validatedHero = validateData(heroValues);
+    console.log(validatedHero);
+
+    if (validatedHero.validData) {
+        const { name, power, energy, age } = heroValues;
+        let newHero = new Hero(name, power, energy, age);
+        console.log(newHero);
+        return newHero;
+    }
+
+    return false;
+};
+
+form.addEventListener('submit', createHero);
